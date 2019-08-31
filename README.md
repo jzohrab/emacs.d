f135e19fab4b68fc2b8f4b72e5a1fd895b67e011
Jeff Zohrab <jzohrab@gmail.com>
2019-08-31 15:23:41 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Emacs settings
+
+For version control of init.el.
+
+Refs:
+
+* https://emacs.stackexchange.com/questions/1/are-there-any-advantages-to-using-emacs-d-init-el-instead-of-emacs
+* https://emacs.stackexchange.com/questions/293/what-should-shouldnt-i-do-when-keeping-emacs-and-emacs-d-in-version-control
---
+++ init.el
@@ -0,0 +1,164 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-modules
+   (quote
+    (org-bbdb org-bibtex org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+
+(require 'package)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(package-initialize)
+
+(require 'helm-config)
+(helm-mode 1)
+
+
+(add-to-list 'load-path "~/.emacs.d/lisp/")
+(require 'php-mode)
+
+
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; ORG MODE
+
+;; https://github.com/sabof/org-bullets
+(require 'org-bullets)
+(add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+(add-hook 'org-mode-hook 'visual-line-mode)
+
+;; Habits
+(setq org-habit-show-habits-only-for-today t)
+(setq org-habit-show-all-today nil)
+
+;; View PDFs in org-mode with docview: links
+(setq doc-view-ghostscript-program "/usr/local/bin/gs")
+(setq doc-view-resolution 128)
+
+;; per note in https://orgmode.org/manual/Activation.html, global keys:
+(global-set-key (kbd "C-c a") 'org-agenda)
+(global-set-key (kbd "C-c c") 'org-capture)
+
+
+;; Capture templates
+;; ref https://www.reddit.com/r/emacs/comments/7zqc7b/share_your_org_capture_templates/
+(setq
+ org-capture-templates
+ '(
+   ("t" "TODO" entry (file+headline "~/Dropbox/org/jz.org" "Collect")
+    "* TODO %? %^G \n  %U" :empty-lines 1)
+   ("s" "Scheduled TODO" entry (file+headline "~/Dropbox/org/jz.org" "Collect")
+    "* TODO %? %^G \nSCHEDULED: %^t\n  %U" :empty-lines 1)
+   ("d" "Deadline" entry (file+headline "~/Dropbox/org/jz.org" "Collect")
+    "* TODO %? %^G \n  DEADLINE: %^t" :empty-lines 1)
+   ("p" "Priority" entry (file+headline "~/Dropbox/org/jz.org" "Collect")
+    "* TODO [#A] %? %^G \n  SCHEDULED: %^t")
+   ("r" "Reviews")
+   ("rd" "Daily" entry (file "~/Dropbox/org/daily_reviews.org")
+    (file "~/Dropbox/org/templates/daily_review.org"))
+   )
+)
+
+;; Custom agenda "d" command - daily agenda
+;; Stolen from https://blog.aaronbieber.com/2016/09/24/an-agenda-for-life-with-org-mode.html
+
+(defun air-org-skip-subtree-if-priority (priority)
+  "Skip an agenda subtree if it has a priority of PRIORITY.
+
+PRIORITY may be one of the characters ?A, ?B, or ?C."
+  (let ((subtree-end (save-excursion (org-end-of-subtree t)))
+        (pri-value (* 1000 (- org-lowest-priority priority)))
+        (pri-current (org-get-priority (thing-at-point 'line t))))
+    (if (= pri-value pri-current)
+        subtree-end
+      nil)))
+
+(defun air-org-skip-subtree-if-habit ()
+  "Skip an agenda entry if it has a STYLE property equal to \"habit\"."
+  (let ((subtree-end (save-excursion (org-end-of-subtree t))))
+    (if (string= (org-entry-get nil "STYLE") "habit")
+        subtree-end
+      nil)))
+
+(setq org-agenda-custom-commands
+      '(("d" "Daily agenda and all TODOs"
+         (
+          (agenda "" ((org-agenda-ndays 1)))
+	  (tags "PRIORITY=\"A\""
+                ((org-agenda-skip-function '(org-agenda-skip-entry-if 'todo 'done))
+                 (org-agenda-overriding-header "High-priority tasks:")))
+          (alltodo ""
+                   ((org-agenda-skip-function '(or (air-org-skip-subtree-if-habit)
+                                                   (air-org-skip-subtree-if-priority ?A)
+                                                   (org-agenda-skip-if nil '(scheduled deadline))))
+                    (org-agenda-overriding-header "Normal priority tasks:")))
+	  )
+         ())))
+
+
+;; More capture shortcuts
+;; ref http://pragmaticemacs.com/emacs/a-shorter-shortcut-to-capture-todo-tasks/
+(defun my-org-capture-todo ()
+  (interactive)
+  "Capture a TODO item"
+  (org-capture nil "t"))
+
+;; bind
+(define-key global-map (kbd "C-t") 'my-org-capture-todo)
+
+;; Tried styling per https://zzamboni.org/post/beautifying-org-mode-in-emacs/,
+;; didn't care for it though.
+
+;; Smaller graphs in agenda.
+;; ref https://orgmode.org/manual/Tracking-your-habits.html
+(setq org-habit-graph-column 65)
+(setq org-habit-preceding-days 10)
+(setq org-habit-following-days 1)
+
+;; ref http://www.newartisans.com/2007/08/using-org-mode-as-a-day-planner/
+(setq org-agenda-start-on-weekday nil)
+(setq org-agenda-show-all-dates t)
+
+(setq org-agenda-files (list "~/Dropbox/org"))  ;; All files in directory
+
+(setq org-list-description-max-indent 5)
+(setq org-ellipsis " ▼")
+
+(setq initial-buffer-choice "~/Dropbox/org/jz.org")
+
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;; keybindings
+(global-set-key "\C-ch" 'query-replace)
+(global-set-key "\C-cr" 'query-replace-regexp)
+
+
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(defun do-count-words (start end)
+	"Print number of words in the region."
+	(interactive "r")
+	(save-excursion
+		(save-restriction
+			(narrow-to-region start end)
+			(goto-char (point-min))
+			(count-matches "\\sw+"))))
+(defun wc () (interactive) (message (number-to-string (do-count-words (point-min) (point-max)))))
+(global-set-key "\C-cw" 'wc)
+
+
+;; ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+(put 'narrow-to-region 'disabled nil)
+(put 'erase-buffer 'disabled nil)
+(setq backup-directory-alist `(("." . "~/.saves")))
+(setq ispell-program-name "/usr/local/Cellar/ispell/3.3.02/bin/ispell")
