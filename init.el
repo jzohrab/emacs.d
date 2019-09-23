37e27c9a202c7c5251d9d8300951d511be892d89
Jeff Zohrab <jzohrab@gmail.com>
2019-09-22 22:58:13 -0400

    Add markdown export

--- init.el
+++ init.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-export-backends (quote (ascii html icalendar latex md odt)))
  '(org-modules
    (quote
     (org-bbdb org-bibtex org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m)))
