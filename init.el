238a51ceb58170ff67f4410f320de35ecabe758b
Jeff Zohrab <jzohrab@gmail.com>
2020-01-18 21:25:40 -0600

    Cont docview

--- init.el
+++ init.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(doc-view-continuous t)
  '(org-export-backends (quote (ascii html icalendar latex md odt)))
  '(org-modules
    (quote
